9c18c850f509bde36e8253aba858e910b9df254c
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-06-09 08:20:25 +0200

    tsc

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,12 @@
-class Department {
-    // private readonly id: string;
-    // private name: string;
-    private employees: string[] = [];
+class Prop {
+  name:string;
+  age:number
   
-    constructor(private readonly id: string, public name: string) {
-      // this.id = id;
-      // this.name = n;
-    }
-  
-    describe(this: Department) {
-      console.log(`Department (${this.id}): ${this.name}`);
-    }
-  
-    addEmployee(employee: string) {
-      // validation etc
-      // this.id = 'd2';
-      this.employees.push(employee);
-    }
-  
-    printEmployeeInformation() {
-      console.log(this.employees.length);
-      console.log(this.employees);
-    }
-  }
-  
-  class ITDepartment extends Department {
-    admins: string[];
-    constructor(id: string, admins: string[]) {
-      super(id, 'IT');
-      this.admins = admins;
-    }
-  }
-  
-  class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[]) {
-      super(id, 'Accounting');
-    }
-  
-    addReport(text: string) {
-      this.reports.push(text);
-    }
-  
-    printReports() {
-      console.log(this.reports);
-    }
-  }
-  
-  const it = new ITDepartment('d1', ['Max']);
-  
-  it.addEmployee('Max');
-  it.addEmployee('Manu');
-  
-  // it.employees[2] = 'Anna';
-  
-  it.describe();
-  it.name = 'NEW NAME';
-  it.printEmployeeInformation();
-  
-  console.log(it);
-  
-  const accounting = new AccountingDepartment('d2', []);
-  
-  accounting.addReport('Something went wrong...');
-  
-  accounting.printReports();
-  
-  // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-  
-  // accountingCopy.describe();
+  constructor(arg:string,n:number){
+  this.name=arg;
+  this.age=n}
+}
 
-  interface Person{
-      name:string;
-      age:number;
+const account=new Prop('Daniela',24)
 
-      greet(phase:string):void
-  }
-
-  let user:Person;
-
-  user={
-      name:'Daniela',
-      age:34,
-      greet(phase:string){
-          console.log(phase + '' + this.name)
-      }
-  }
-  user.greet('Hello, my name is')
+console.log(account)
