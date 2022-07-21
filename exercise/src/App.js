f0805b3f1768246045b34ec4aa125f751261370b
Daniela Karwowska <daniela.karwowska@o2.pl>
2022-07-21 12:04:41 +0200

    simple dropdown

---
+++ exercise/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dropdown from './components/dropdown'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Dropdown</h1>
+    <Dropdown/>
+    </div>
+  );
+}
+
+export default App;
---
+++ exercise/src/components/dropdown.jsx
@@ -0,0 +1,12 @@
+const Dropdown = () => {
+    return(
+        <form>
+  <label>
+    Imię:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Wyślij" />
+</form>
+    )
+}
+export default Dropdown
